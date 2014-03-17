022454ef9a37bce9f2a5095536b0fc5072829d5b
sbryzak <sbryzak@gmail.com>
2014-03-17 17:03:38 +1000

    numerous improvements to menu bar

--- src/main/resources/org/xwidgets/core/menubar.js
+++ src/main/resources/org/xwidgets/core/menubar.js
@@ -6,18 +6,17 @@ org.xwidgets.core.MenuItem = xw.Visual.extend({
     this.registerProperty("label", {default: label});
     this.registerProperty("rendered");
     this.registerProperty("styleClass", {default: "xw_menuitem"});
+    this.registerProperty("selectedStyleClass", {default: "xw_menuitem_selected"});
     this.registerProperty("submenuStyleClass", {default: "xw_submenu"});
     this.registerProperty("definition", {elListener: this.updateDefinition});
     this.registerEvent("onclick");
     this.control = null;
     this.submenuContainer = null;
-    this.submenuOpen = false;
   },
   updateDefinition: function(def) {
     // If the submenu is open, close it
     if (this.submenuOpen) {
-      this.submenuContainer.style.display = "none";
-      this.submenuOpen = false;
+      this.unselect();
     }
     // Clear all the current children - TODO move this to the base Widget class
     if (xw.Sys.isDefined(this.childen)) {
@@ -26,13 +25,13 @@ org.xwidgets.core.MenuItem = xw.Visual.extend({
       }
     }
   
-    this.children = this.parseDefinition(def, this);
+    this.children = this.parseDefinition(def, this);    
   },
   parseDefinition: function(nodes, parent) {
     var items = [];
     for (var node in nodes) {
       var mi = new org.xwidgets.core.MenuItem(nodes[node].label);
-      mi.setParent(parent);    
+      mi.setParent(parent); 
       
       if (nodes[node].children) {
         mi.children = this.parseDefinition(nodes[node].children, mi);
@@ -61,23 +60,57 @@ org.xwidgets.core.MenuItem = xw.Visual.extend({
       container.appendChild(this.control);
       
       var that = this;
+
       var clickEvent = function(event) {
         that.click(event);
       }
+            
       xw.Sys.chainEvent(this.control, "click", clickEvent);
+      
+      // Disable default text selection behaviour
+      var mouseDownEvent = function(event) {
+        xw.Sys.cancelEventBubble(event);
+      };
+      xw.Sys.chainEvent(this.control, "mousedown", mouseDownEvent);
+      
+      if (this.children.length > 0) {
+        var mouseOverEvent = function(event) {
+          that.mouseOver(event);
+        }
+        xw.Sys.chainEvent(this.control, "mouseover", mouseOverEvent);
+      }
     }
   },
   click: function(event) {
-    if (this.children.length > 0) {
-      this.toggleSubmenu();
-    } else if (this.onclick) {
-      this.onclick(event);
+    var menuBar = this.getMenuBar();
+    menuBar.selectItem(this);
+    if (this.children.length == 0) {
+      if (this.onclick) {
+        menuBar.close();
+        this.onclick(event);
+      } else if (menuBar && menuBar.onclick) {
+        menuBar.close();
+        menuBar.onclick.invoke(menuBar, {item: this});
+      }
     }
+    xw.Sys.cancelEventBubble(event);
   },
-  toggleSubmenu: function() {
-    if (this.submenuOpen) {
-      this.submenuContainer.style.display = "none";
-    } else {
+  mouseOver: function(event) {
+    if (this.getMenuBar().isOpen()) {
+      this.getMenuBar().trackMouseOver(this);
+    }
+    
+    xw.Sys.cancelEventBubble(event);
+  },
+  getMenuBar: function() {
+    var p = this.parent;
+    while (!(p instanceof org.xwidgets.core.MenuBar) && xw.Sys.isDefined(p)) {
+      p = p.parent;
+    }
+    return p;
+  },
+  select: function() {
+    if (this.children.length > 0) {
       var c = this.submenuContainer;
       if (c == null) {
         c = document.createElement("div");
@@ -90,19 +123,34 @@ org.xwidgets.core.MenuItem = xw.Visual.extend({
         
         var rect = this.control.getBoundingClientRect();        
 
-        c.style.top = (rect.bottom + 1) + "px";  
-        c.style.left = rect.left + "px";
+        if (this.parent instanceof org.xwidgets.core.MenuBar) {
+          c.style.top = (rect.bottom + 1) + "px";  
+          c.style.left = rect.left + "px";
+        } else {
+          var containerRect = this.parent.submenuContainer.getBoundingClientRect();
+          c.style.top = rect.top + "px";
+          c.style.left = (containerRect.right + 1) + "px";
+        }
         c.style.overflow = "hidden";
         
         this.renderChildren(c);
         
         document.body.appendChild(c);
-        this.submenuContainer = c;
-      } else {
-        this.submenuContainer.style.display = "";
+        this.submenuContainer = c; 
       }
+      this.submenuContainer.style.display = "";
     }
-    this.submenuOpen = !this.submenuOpen;
+    if (xw.Sys.isDefined(this.selectedStyleClass)) {
+      this.control.className = this.selectedStyleClass;
+    }
+  },
+  unselect: function() {
+    if (this.submenuContainer) {
+      this.submenuContainer.style.display = "none";  
+    }
+    if (xw.Sys.isDefined(this.styleClass)) {
+      this.control.className = this.styleClass;
+    }
   }
 });
 
@@ -110,7 +158,10 @@ org.xwidgets.core.MenuBar = xw.Visual.extend({
   _constructor: function() {
     this._super();
     this.registerProperty("styleClass", {default: "xw_menubar"});
+    this.registerEvent("onclick");
     this.control = null;    
+    this.selectedMenuItem = null;
+    this.mouseOverItem = null;
   },
   render: function(container) {
     if (this.control == null) {
@@ -121,7 +172,81 @@ org.xwidgets.core.MenuBar = xw.Visual.extend({
       
       container.appendChild(this.control);      
     }
-    
     this.renderChildren(this.control);
+  },
+  trackMouseOver: function(menuItem) {
+    if (menuItem != this.mouseOverItem) {
+      // Clear the existing timeout function if it's set
+      if (this.mouseOverTimeout) {
+        clearTimeout(this.mouseOverTimeout);
+        this.mouseOverTimeout = null;
+      }
+      
+      // Set the new item
+      this.mouseOverItem = menuItem;
+      
+      // Create a new timeout
+      var that = this;
+      var cb = function() {
+        that.mouseOverCallback(menuItem);      
+      }
+      this.mouseOverTimeout = setTimeout(cb, 300);
+    }
+  },
+  mouseOverCallback: function(menuItem) {
+    if (menuItem == this.mouseOverItem && menuItem != this.selectedMenuItem) {
+      this.selectItem(menuItem);
+    }
+  },
+  selectItem: function(menuItem) {
+    var open = this.isOpen();
+    
+    if (this.selectedMenuItem != null && 
+        this.selectedMenuItem != menuItem && 
+        menuItem.parent != this.selectedMenuItem) {
+      var i = this.selectedMenuItem;
+      while (i != menuItem.parent) {
+        i.unselect();
+        i = i.parent;
+      } 
+    } 
+    
+    if (this.selectedMenuItem == menuItem) {
+      menuItem.unselect();
+      this.selectedMenuItem = this.selectedMenuItem.parent != this ? this.selectedMenuItem.parent : null;
+    } else {
+      menuItem.select();
+      this.selectedMenuItem = menuItem;
+    }
+    
+    // If the menu wasn't open before but is down, chain a mousedown event on the document body 
+    // that will close the menu
+    if (!open && this.isOpen()) {
+      org.xwidgets.core.MenuBar.openMenu = this;
+      xw.Sys.chainEvent(document.body, "mousedown", org.xwidgets.core.MenuBar.documentMouseDown);
+    }
+  },
+  isOpen: function() {
+    return this.selectedMenuItem != null;
+  },
+  close: function() {
+    if (this.selectedMenuItem != null) {
+      var i = this.selectedMenuItem;
+      while (i != this) {
+        i.unselect();
+        i = i.parent;
+      }
+    
+      this.selectedMenuItem = null;
+    }
   }
+  
 });
+
+org.xwidgets.core.MenuBar.openMenu = null;
+
+org.xwidgets.core.MenuBar.documentMouseDown = function(event) {
+  org.xwidgets.core.MenuBar.openMenu.close();
+  xw.Sys.unchainEvent(document.body, "mousedown", org.xwidgets.core.MenuBar.documentMouseDown);
+  xw.Sys.cancelEventBubble(event);
+};
