fb53c8f48415812bb77db84779edb7f6a814c8ab
sbryzak <sbryzak@gmail.com>
2014-05-09 13:04:06 +1000

    add support for splitter resizing

--- src/main/resources/org/xwidgets/core/splitter.js
+++ src/main/resources/org/xwidgets/core/splitter.js
@@ -4,11 +4,12 @@ org.xwidgets.core.Splitter = xw.Visual.extend({
   _constructor: function() {
     this._super();
     this.registerProperty("orientation", {default: "vertical"});
+    this.registerProperty("splitterClass", {default: null});
     this.control = null;
     this.first = null;
     this.second = null;
     this.splitter = null;
-  },  
+  },
   render: function(container) {
     if (this.control == null) {
       this.control = document.createElement("div");
@@ -19,19 +20,27 @@ org.xwidgets.core.Splitter = xw.Visual.extend({
       this.first.style.cssFloat = "left";
       this.first.style.height = "100%";
       this.first.style.width = "300px";
+      this.first.style.overflow = "auto";
       this.control.appendChild(this.first);
       
       this.splitter = document.createElement("div");
       this.splitter.style.cssFloat = "left";
       this.splitter.style.height = "100%";
-      this.splitter.style.width = "5px";
+      if (this.splitterClass.value != null) {
+        this.splitter.className = this.splitterClass.value; 
+      } else {
+        this.splitter.style.width = "5px";
+      }
       this.control.appendChild(this.splitter);
+      var that = this;
+      var cb = function(event) {
+        that.startDragging(event);
+      };
+      xw.Sys.chainEvent(this.splitter, "mousedown", cb);
       
       this.second = document.createElement("div");
-//      this.second.style.cssFloat = "left";
       this.second.style.overflow = "hidden";
       this.second.style.height = "100%";
-//      this.second.style.width = "400px";
       this.control.appendChild(this.second);
 
       if (this.children.length != 2) {
@@ -46,6 +55,32 @@ org.xwidgets.core.Splitter = xw.Visual.extend({
         this.renderChild(this.children[1], this.second);
       }
     }
+  },
+  startDragging: function(event) {
+    this.startDragPosition = event.clientX;
+    this.startFirstWidth = this.first.offsetWidth;
+    
+    var that = this;
+    this.mouseUpCallback = function(event) {
+      that.stopDragging(event);
+    };
+    this.mouseMoveCallback = function(event) {
+      that.drag(event);
+    };
+    xw.Sys.chainEvent(document, "mouseup", this.mouseUpCallback);
+    xw.Sys.chainEvent(document, "mousemove", this.mouseMoveCallback);
+  },
+  drag: function(event) {
+    var delta = this.startDragPosition - event.clientX;
+    var newWidth = this.startFirstWidth - delta;
+    if (newWidth > 20) {
+      this.first.style.width = newWidth + "px";
+    }
+    event.preventDefault();
+  },
+  stopDragging: function(event) {
+    xw.Sys.unchainEvent(document, "mouseup", this.mouseUpCallback);
+    xw.Sys.unchainEvent(document, "mousemove", this.mouseMoveCallback);
   },
   destroy: function() {
     
