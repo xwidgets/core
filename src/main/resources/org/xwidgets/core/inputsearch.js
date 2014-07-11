9549228eca0286ec085ea04479ef0b3f3575c35f
sbryzak <sbryzak@gmail.com>
2014-07-11 10:31:32 +0800

    improvements to inputsearch

--- src/main/resources/org/xwidgets/core/inputsearch.js
+++ src/main/resources/org/xwidgets/core/inputsearch.js
@@ -1,67 +1,63 @@
 package("org.xwidgets.core");
 
-org.xwidgets.core.InputSearch = function() {
-  xw.Visual.call(this);
-  this._className = "org.xwidgets.core.InputSearch"; 
-  this.registerProperty("value", null);
-  this.registerProperty("styleClass", "");
-  this.registerEvent("onsearch", null);
-  this.control = null;
-};
+org.xwidgets.core.InputSearch = xw.Visual.extend({
+  _constructor: function() {
+    this._super(false);
+    this.registerProperty("value", {default: null});    
+    this.registerProperty("placeholder", {default: null});
+    this.registerProperty("styleClass", {default: null});
+    this.registerEvent("onsearch");
+    this.control = null;
+  },
+  getValue: function() {
+    return this.control.value;
+  },
+  render: function(container) {
+    if (this.control == null) {
+      var that = this;
 
-org.xwidgets.core.InputSearch.prototype = new xw.Visual();
-   
-org.xwidgets.core.InputSearch.prototype.render = function(container) {
-  if (this.control == null) {
-    
-    var that = this;
+      this.control = document.createElement("input");
+      this.control.type = "text";
 
-    this.control = document.createElement("input");    
-    this.control.type = "text";
-    this.control.className = this.styleClass;
-    if (this.value.value != null) {
-      this.control.value = this.value.value;
+      if (this.styleClass.value != null) {
+        this.control.className = this.styleClass.value;
+      }
+
+      if (this.value.value != null) {
+        this.control.value = this.value.value;
+      }
+      
+      if (this.placeholder.value != null) {
+        this.control.placeholder = this.placeholder.value;
+      }
+      
+      var that = this;
+      this.control.onkeypress = function() {
+        that.checkEnter(event);
+      };
+      
+      //var inp = document.createElement("input");
+      //inp.type = "submit";
+      //inp.style.display = "none";
+      
+      container.appendChild(this.control);
+    }
+  },
+  checkEnter: function(event) {
+    var key;
+    if (window.event) {
+      key = window.event.keyCode;
+    } else if (event) {
+      key = event.which;
     }
     
-    var that = this;
-    this.control.onkeypress = function() {
-      that.checkEnter(event);
-    };
-    
-    var inp = document.createElement("input");
-    inp.type = "submit";
-    inp.style.display = "none";
-    
-    container.appendChild(this.control);
-  }    
-};
-
-org.xwidgets.core.InputSearch.prototype.checkEnter = function(event) {
-  var key;
-  if (window.event) {
-    key = window.event.keyCode;
-  } else if (event) {
-    key = event.which;
+    if (key == 13) {
+      if (xw.Sys.isDefined(this.onsearch)) {
+        this.onsearch.invoke(this, {searchTerm: this.getValue()});
+      }
+    }    
+  },
+  toString: function() {
+    return "org.xwidgets.core.InputSearch[" + this.id.value + "]";
   }
-  
-  if (key == 13) {
-    if (xw.Sys.isDefined(this.onsearch)) {
-      this.onsearch.invoke(this, {searchTerm: this.getValue()});
-    }
-  }
-};
-
-org.xwidgets.core.InputSearch.prototype.getValue = function() {
-  return this.control.value;
-};
-
-org.xwidgets.core.InputSearch.prototype.setValue = function(value) {
-  this.value.value = value;
-  if (this.control != null) {
-    this.control.value = this.value.value;
-  }
-};
-
-org.xwidgets.core.InputSearch.prototype.toString = function() {
-  return "org.xwidgets.core.InputSearch[" + this.id.value + "]";
-};
+});
