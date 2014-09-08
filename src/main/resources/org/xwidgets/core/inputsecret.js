505de8cb7b598df59b4a6e61011c0e6288da0cff
sbryzak <sbryzak@gmail.com>
2014-09-08 17:09:05 +1000

    added formData support to inputSecret

--- src/main/resources/org/xwidgets/core/inputsecret.js
+++ src/main/resources/org/xwidgets/core/inputsecret.js
@@ -3,7 +3,9 @@ package("org.xwidgets.core");
 org.xwidgets.core.InputSecret = xw.Visual.extend({
   _constructor: function() {
     this._super();
+    this.registerProperty("name", {default: null});
     this.registerProperty("value", {default: null});
+    this.registerProperty("formData", {default: null});
     this.registerProperty("styleClass", {default: ""});
     this.control = null;
   },  
@@ -11,9 +13,23 @@ org.xwidgets.core.InputSecret = xw.Visual.extend({
     if (this.control == null) {
       this.control = document.createElement("input");
       this.control.type = "password";
+      var that = this;
+      var cb = function(evt) {
+        that.checkValueChanged.call(that, evt);
+      }
+      xw.Sys.chainEvent(this.control, "keyup", cb);
       this.control.className = this.styleClass;
       container.appendChild(this.control);
     }    
+  },
+  checkValueChanged: function(event) {
+    var value = this.control.value;
+    if (value != this.value.value) {
+      this.value.value = value;
+      if (this.formData.value != null) {
+        this.formData.value.updateValue(this.name.value, this.value.value);
+      }
+    }
   },
   getValue: function() {
     return this.control.value;
